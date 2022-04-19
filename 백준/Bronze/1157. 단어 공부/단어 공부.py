f59485311e42446140a8cc73a102626946d71b67
김 지용 <74017167+JerryKim1023@users.noreply.github.com>
2022-04-19 12:03:37 +0900

    [Bronze I] Title: 단어 공부, Time: 104 ms, Memory: 32796 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,13 +1,13 @@
-word = input().upper()
-word_list = list(set(word))
-
-cnt = []
-for i in word_list:
-  count = word.count
-  cnt.append(count(i))
-
-if cnt.count(max(cnt)) > 1:
-  print("?")
-
+words = input().upper()   
+setting_words = list(set(words))
+# ex)ariranga    /   setting_words = aring    cnt_list = 3 2 1 1 1 
+cnt_list = []
+for i in setting_words:
+    cnt = words.count(i)
+    cnt_list.append(cnt)
+    
+if cnt_list.count(max(cnt_list)) > 1:
+    print('?')
 else:
-  print(word_list[(cnt.index(max(cnt)))])
+    max_index = cnt_list.index(max(cnt_list))
+    print(setting_words[max_index])
